c8d816c8377d4b966c22a431519d6dcb054bb8be
KELMAN NICOLAS <47297850@ort>
2022-09-28 14:01:57 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link href = "menuprincipal.css" rel = "stylesheet">
+
+</head>
+
+<body style = "background-image: url('image 1.png');">
+
+<div class="header">
+
+  <div class="logo">
+    <img src = "Captura 1.png" height = 150  width = 300>
+  </div>
+
+  <nav class="navbar">
+    <a href="index.php">Inicio</a>
+    <a href="lineas.html">Lineas</a>
+    <a href="Ayuda.html">Ayuda</a>
+  </nav>
+
+  <div class= "Metrovia">
+    <p>
+    Metrovía
+    </p>
+  </div>
+
+</div>
+
+<main>
+
+<div class= "LineasDesplegables">
+  <section1 class= "LineaDesplegable1">
+    <div class="RecGris1"></div>
+    <div class="RecBlanco1"></div>
+    <span  class="txtLinea">Linea:</span>
+  </section1>
+
+  <section2 class= "LineaDesplegable2">
+    <div class="RecGris2"></div>
+    <div class="RecBlanco2"></div><span  class="txtDesde">Desde:</span>
+  </section2>
+
+  <section3 class= "LineaDesplegable3">
+    <div class="RecGris3"></div>
+    <div class="RecBlanco3"></div><span  class="txtHacia">Hacia:</span>
+  </section3>
+</div>
+
+<div class="SeleccioneLin">
+  <span  class="SeleccioneLin"><a href="#"><span class="ListaL">Seleccione...</a></span>
+</div>
+
+<div class="SeleccioneDes">
+  <span  class="SeleccioneDes"><a href="#"><span class="ListaD">Seleccione...</a></span>
+</div>
+
+<div class="SeleccioneHac">
+  <span  class="SeleccioneHac"><a href="#"><span class="ListaH">Seleccione...</a></span>
+</div>
+
+<div class= "btnContinuar">
+  <div class="btn"></div><a href="Proximotren.html"><span class="Continuar">CONTINUAR</span></a>
+</div>
+
+  <div class= "A">
+    <ul>
+      <li><img src = "lineas/Subte A.png"></li>  
+      <li><img src = "lineas/Subte B.png"></li>
+      <li><img src = "lineas/Subte C.png"></li>
+      <li><img src = "lineas/Subte D.png"></li>
+      <li><img src = "lineas/Subte E.png"></li>
+      <li><img src = "lineas/Subte H.png"></li>
+   </ul>
+  </div>
+
+</main>
+
+</body>
+</html>
